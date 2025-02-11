3e2e7ef848543ac8b07d8df126dd3e3b6bb3d2f1
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-02-11 12:12:32 -0800

    Completed ex05

--- dist/ex05.js
+++ dist/ex05.js
@@ -1,8 +1,7 @@
+"use strict";
 // 5. Create a generic arrow function called `filterArray` that filters an array based on a callback function.
 // Pass an array of numbers [1, 2, 3, 4, 5] and filter for numbers greater than 2.
 // Log the filtered array.
-
-const filterArray = <T, U>(arr: T[], callback: (el: T) => U): T[] => arr.filter(callback); // T: number, T[]: number[], U: boolean
-
+const filterArray = (arr, callback) => arr.filter(callback); // T: number, T[]: number[], U: boolean
 console.log(filterArray([1, 2, 3, 4, 5], (n) => n > 2)); // Expected output: [3, 4, 5]
-console.log(filterArray([10, 24, 33, 42, 57], (n) => n%2)); // Expected output: [33, 57]
+console.log(filterArray([10, 24, 33, 42, 57], (n) => n % 2)); // Expected output: [33, 57]
